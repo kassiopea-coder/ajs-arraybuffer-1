070cb0e792aeb6e77f7e074b787e6d2e1ae93c69
Nadezhda Zizevskaya <7_nadezhda@mail.ru>
2021-07-11 18:27:09 +0400

    first commit

---
+++ src/js/__tests__/Daemon.test.js
@@ -0,0 +1,13 @@
+import Character from '../Daemon';
+
+test('create Character ok', () => {
+  const character = new Character('Harry', 'Daemon');
+  const result = {
+    name: 'Harry',
+    type: 'Daemon',
+    health: 100,
+    level: 1,
+    defence: 40,
+  };
+  expect(character).toMatchObject(result);
+});
